436e168979e8f124ccc714985d10c1c1c3c63429
DijitalDonusumAkademi <dijital.donusum@yahoo.com>
2020-08-25 18:44:29 +0300

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using Data.Configuration;
+using Data.Seed;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductSeed(new int[] {1, 2}));
+            modelBuilder.ApplyConfiguration(new CategorySeed(new int[] {1, 2}));
+        }
+    }
+}
